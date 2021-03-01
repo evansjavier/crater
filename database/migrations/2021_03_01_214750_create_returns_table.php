fd3a26222001cb3da1ce8934cc1a531c634e2517
Javier Evans <evansjavier16@gmail.com>
2021-03-01 18:05:13 -0400

    wip: se agregan tablas para devoluciones

---
+++ database/migrations/2021_03_01_214750_create_returns_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReturnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('returns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('return_date');
+            // $table->date('due_date');
+            $table->string('return_number');
+            $table->string('reference_number')->nullable();
+            $table->string('status');
+            // $table->string('paid_status');
+            // $table->string('tax_per_item');
+            // $table->string('discount_per_item');
+            $table->text('notes')->nullable();
+            // $table->string('discount_type')->nullable();
+            // $table->decimal('discount', 15, 2)->nullable();
+            // $table->unsignedBigInteger('discount_val')->nullable();
+            // $table->unsignedBigInteger('sub_total');
+            $table->unsignedBigInteger('total');
+            // $table->unsignedBigInteger('tax');
+            // $table->unsignedBigInteger('due_amount');
+            $table->boolean('sent')->default(false);
+            $table->boolean('viewed')->default(false);
+            $table->string('unique_hash')->nullable();
+            // $table->integer('invoice_template_id')->unsigned()->nullable();
+            // $table->foreign('invoice_template_id')->references('id')->on('invoice_templates');
+            
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedInteger('company_id')->nullable();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->unsignedInteger('creator_id')->nullable();
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('returns');
+    }
+}
